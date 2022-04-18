75f720ff461d0e20a53dd5d367866563dd2b3eaa
563413632 <563413632@qq.com>
2022-04-18 09:56:45 +0800

    gai

--- HomeWork/js_day10_张韵然_2022-4-15/js/4-15.js
+++ HomeWork/js_day10_张韵然_2022-4-15/js/4-15.js
@@ -1,5 +1,10 @@
 var phone = document.getElementById('phone');
+var a = document.getElementById('a');
 phone.onmouseenter = function () {
+    var navRight = document.getElementById('navRight');
+    navRight.classList.add('active')
+}
+a.onmouseenter = function () {
     var navRight = document.getElementById('navRight');
     navRight.classList.add('active')
 }
